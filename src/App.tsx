a5e5ade37c8eaa028dd1f03b017139eae1e9992e
Joseph Wan <joseph.h.wan@gmail.com>
2023-05-24 16:31:02 -0500

    fix: pull out grid elements

--- src/App.tsx
+++ src/App.tsx
@@ -7,71 +7,71 @@ function App() {
   return (
     <div className="App">
       <div className="flex flex-col p-24">
-        <div className="container m-auto grid grid-cols-8 grid-rows-10 gap-4">
+        <div className="container m-auto grid grid-cols-8 gap-4">
           <HeaderBox name="USER" />
           <HeaderBox name="WEB APPLICATION" />
           <HeaderBox name="AUTHORIZATION LAYER" />
           <HeaderBox name="YOUR API" />
-          <Step
-            col_start={2}
-            col_span={2}
-            order={1}
-            arrow="right"
-          />
-          <Step
-            col_start={4}
-            col_span={2}
-            order={2}
-            arrow="right"
-          />
-          <Step
-            col_start={2}
-            col_span={4}
-            order={3}
-            arrow="left"
-          />
-          <Step
-            col_start={2}
-            col_span={4}
-            order={4}
-            arrow="right"
-          />
-          <Step
-            col_start={4}
-            col_span={2}
-            order={5}
-            arrow="left"
-          />
-          <Step
-            col_start={4}
-            col_span={2}
-            order={6}
-            arrow="right"
-          />
-          <Step
-            col_start={4}
-            col_span={2}
-            order={7}
-            arrow="left"
-          />
-          <Step
-            col_start={4}
-            col_span={2}
-            order={8}
-            arrow="left"
-          />
-          <Step
-            col_start={4}
-            col_span={4}
-            order={9}
-            arrow="right"
-          />
-          <Step
-            col_start={4}
-            col_span={4}
-            order={10}
-            arrow="left"
-          />
+          <div className={"row-span-1 row-start-2 col-start-2 col-span-2"}>
+            <Step
+              order={1}
+              arrow="right"
+            />
+          </div>
+          <div className={"row-span-1 row-start-3 col-start-4 col-span-2"}>
+            <Step
+              order={2}
+              arrow="right"
+            />
+          </div>
+          <div className={"row-span-1 row-start-4 col-start-2 col-span-4"}>
+            <Step
+              order={3}
+              arrow="left"
+            />
+          </div>
+          <div className={"row-span-1 row-start-5 col-start-2 col-span-4"}>
+            <Step
+              order={4}
+              arrow="right"
+            />
+          </div>
+          <div className={"row-span-1 row-start-6 col-start-4 col-span-2"}>
+            <Step
+              order={5}
+              arrow="left"
+            />
+          </div>
+          <div className={"row-span-1 row-start-7 col-start-4 col-span-2"}>
+            <Step
+              order={6}
+              arrow="right"
+            />
+          </div>
+          <div className={"row-span-1 row-start-8 col-start-4 col-span-2"}>
+            <Step
+              order={7}
+              arrow="left"
+            />
+          </div>
+          <div className={"row-span-1 row-start-9 col-start-4 col-span-2"}>
+            <Step
+              order={8}
+              arrow="left"
+            />
+          </div>
+          <div className={"row-span-1 row-start-10 col-start-4 col-span-4"}>
+            <Step
+              order={9}
+              arrow="right"
+            />
+          </div>
+          <div className={"row-span-1 row-start-11 col-start-4 col-span-4"}>
+            <Step
+              order={10}
+              arrow="left"
+            />
+          </div>
         </div>
       </div>
     </div>
--- src/components/Step.tsx
+++ src/components/Step.tsx
@@ -4,14 +4,12 @@ import RightArrow from './RightArrow'
 import DoubleArrow from './DoubleArrow'
 
 export interface StepInterface {
-  col_start: number,
-  col_span: number,
   order: number,
   arrow: "right" | "left" | "both" 
 }
-export default function Step({col_start, col_span, order, arrow}:StepInterface) {
+export default function Step({order, arrow}:StepInterface) {
   return (
-    <div className={`row-span-1 row-start-${order + 1} col-start-${col_start} col-span-${col_span}`}>
+    <div>
       { arrow === "right" && <RightArrow order={order} /> }
       { arrow === "both" && <DoubleArrow order={order} /> }
       { arrow === "left" && <LeftArrow order={order} /> }
