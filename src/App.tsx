aab9ad79984389d2fcf193994304abf6d703fc1e
Joseph Wan <joseph.h.wan@gmail.com>
2023-05-25 16:27:12 -0500

    feat: increase rows to 11

--- src/App.tsx
+++ src/App.tsx
@@ -12,61 +12,61 @@ function App() {
           <HeaderBox name="WEB APPLICATION" />
           <HeaderBox name="AUTHORIZATION LAYER" />
           <HeaderBox name="YOUR API" />
-          <div className={"row-span-1 row-start-2 col-start-2 col-span-2"}>
+          <div className="row-span-1 row-start-2 col-start-2 col-span-2">
             <Step
               order={1}
               arrow="right"
             />
           </div>
-          <div className={"row-span-1 row-start-3 col-start-4 col-span-2"}>
+          <div className="row-span-1 row-start-3 col-start-4 col-span-2">
             <Step
               order={2}
               arrow="right"
             />
           </div>
-          <div className={"row-span-1 row-start-4 col-start-2 col-span-4"}>
+          <div className="row-span-1 row-start-4 col-start-2 col-span-4">
             <Step
               order={3}
               arrow="left"
             />
           </div>
-          <div className={"row-span-1 row-start-5 col-start-2 col-span-4"}>
+          <div className="row-span-1 row-start-5 col-start-2 col-span-4">
             <Step
               order={4}
               arrow="right"
             />
           </div>
-          <div className={"row-span-1 row-start-6 col-start-4 col-span-2"}>
+          <div className="row-span-1 row-start-6 col-start-4 col-span-2">
             <Step
               order={5}
               arrow="left"
             />
           </div>
-          <div className={"row-span-1 row-start-7 col-start-4 col-span-2"}>
+          <div className="row-span-1 row-start-7 col-start-4 col-span-2">
             <Step
               order={6}
               arrow="right"
             />
           </div>
-          <div className={"row-span-1 row-start-8 col-start-4 col-span-2"}>
+          <div className="row-span-1 row-start-8 col-start-4 col-span-2">
             <Step
               order={7}
               arrow="left"
             />
           </div>
-          <div className={"row-span-1 row-start-9 col-start-4 col-span-2"}>
+          <div className="row-span-1 row-start-9 col-start-4 col-span-2">
             <Step
               order={8}
               arrow="left"
             />
           </div>
-          <div className={"row-span-1 row-start-10 col-start-4 col-span-4"}>
+          <div className="row-span-1 row-start-10 col-start-4 col-span-4">
             <Step
               order={9}
               arrow="right"
             />
           </div>
-          <div className={"row-span-1 row-start-11 col-start-4 col-span-4"}>
+          <div className="row-span-1 row-start-11 col-start-4 col-span-4">
             <Step
               order={10}
               arrow="left"
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        '11': 'repeat(11, minmax(0, 1fr))',
+      },
+      gridRowStart: {
+        '8': '8',
+        '9': '9',
+        '10': '10',
+        '11': '11',
+      },
+      gridRowEnd: {
+        '8': '8',
+        '9': '9',
+        '10': '10',
+        '11': '11',
+      }
+    },
   },
   plugins: [],
 }
